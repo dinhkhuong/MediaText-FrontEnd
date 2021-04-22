45b3e7b15cf0eb88e3749bb638113a3c7253e6d3
adam <everett.ingram22@gmail.com>
2021-04-22 12:29:14 -0400

    Ending Source

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import './bootstrap.css';
 import './App.css';
+import RouterComponet from './component/general/RouterComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+  •	npm add axios
+  •	npm add react-router-dom
+  •	npm add formik 
+*/
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <RouterComponet/> 
+      </div>
+    )
+  }
 }
 
 export default App;
